465ff5a6236c1e8f7e1057fb8022065d59a846bf
maxudka <max.kalashnik@gmail.com>
2016-06-06 18:30:29 +0300

    06.06

---
+++ 19/js/script.js
@@ -0,0 +1,7 @@
+var student = "Максим";
+console.info(student);
+student='Калашник';
+console.info(student);
+var name_of_user = prompt('Введите свое имя','Вводите здесь');
+alert('Добро пожаловать, '+name_of_user);
+confirm('Вы и правда хотите загрузить эту страницу?');
