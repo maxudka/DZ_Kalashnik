2e21c1bb285ac0cb1bd052a9c8fa6285a65c48fb
maxudka <max.kalashnik@gmail.com>
2016-06-09 00:20:12 +0300

    09.06

---
+++ 20/script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by maxud on 08.06.2016.
+ */
+
+/* 1 */
+var month = prompt("Какой сейчас месяц?");
+if (month == "сентябрь")
+{ alert ("Вы правы!")}
+else { alert ("Та ну ты что? сентябрь!")};
+
+/* 2 */
+var num = +prompt('Введите любое целое число?');
+console.info (num);
+if (num > 0)
+{alert(1)}
+else if (num < 0)
+{alert(-1)}
+else {alert(0)}
+
+/* 3 */
+x + y >= 9 ? alert ('Да!') : alert ('Нет!');
+
+/* 4 */
+var i = 0;
+while ( ++i > 0 && Num <= 100) { Num = +prompt('Введите число')}
+/* не понял, зачем тут функция?) */
+
+/* 5 */
